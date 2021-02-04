739ccebc8f6890ad7c8e6079cae5bb2381ec4840
Kurai András <k.andras9642@gmail.com>
2021-02-04 19:00:03 +0100

    add benchmark

---
+++ NumericsConverter.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace NumericsConverter.Benchmark
+{
+    public class Program
+    {
+        [System.Obsolete]
+        public static void Main() =>
+            BenchmarkRunner.Run<VectorBenchmark>(
+                ManualConfig
+                    .Create(DefaultConfig.Instance)
+                    .With(ConfigOptions.DisableOptimizationsValidator));
+    }
+}
---
+++ NumericsConverter.Benchmark/VectorBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace NumericsConverter.Benchmark
+{
+    [SimpleJob(RuntimeMoniker.Net472)]
+    [MemoryDiagnoser]
+    public class VectorBenchmark
+    {
+        [Benchmark]
+        public void Alloc()
+        {
+            for (var i = 0; i < 100000; i++)
+            {
+                ConvertAlloc(new UnityEngine.Vector4(1f, 2f, 3f, 4f));
+            }
+        }
+
+        [Benchmark]
+        public void NoAlloc()
+        {
+            for (var i = 0; i < 100000; i++)
+            {
+                new UnityEngine.Vector4(1f, 2f, 3f, 4f).ToSystemGeneric();
+            }
+        }
+
+        public static System.Numerics.Vector<float> ConvertAlloc(UnityEngine.Vector4 vector)
+        {
+            var array = new float[System.Numerics.Vector<float>.Count];
+            array[0] = vector.x;
+            array[1] = vector.y;
+            array[2] = vector.z;
+            array[3] = vector.w;
+
+            return new System.Numerics.Vector<float>(array);
+        }
+    }
+}
